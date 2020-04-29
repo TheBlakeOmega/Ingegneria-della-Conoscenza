e3f3d203f3f0056ce3ea13c428b263dc98d57987
Vincenzo Belvedere <vincenzo.belvedere.99@gmail.com>
2020-04-29 16:53:10 +0200

    modifica organizzazione filesù

--- dataFunctions.py
+++ dataFunctions.py
@@ -2,32 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 def getData(path):
     data = pd.read_csv(path)
-    data = data.drop('filename', 1)
-    data = data.drop('tempo', 1)
-    datas = np.split(data, [27], axis=1)
-    train = datas[0]
-    test = datas[1]
-    X_train, X_test, Y_train, Y_test = train_test_split(train, test, test_size=0.2)
+    datas = data.drop('filename', 1)
+    X_data = np.array(datas.drop('label', 1))
+    y_data = np.array(data['label'])
+    scaler = StandardScaler()
+    X_data = scaler.fit_transform(X_data)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_data, y_data, test_size=0.2, shuffle='true',stratify=y_data)
     return X_train, X_test, Y_train, Y_test
-
-
-def makePCA(train, test):
-    print("TRAIN SIZE BEFORE PCA:", train.shape)
-    pca = PCA(n_components=5, whiten=True)
-    pca.fit(train)
-    train = pca.transform(train)
-    print("TRAIN SIZE AFTER PCA:", train.shape)
-    test = pca.transform(test)
-    return train, test, pca
-
-
-
-
-
-
 
 
 
